eccc07e845529b456458846fba6fcc9ee3244b61
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-29 23:42:18 +0900

    Cin 입력 예시

---
+++ BasicSyntax/IO/CinEx.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std; 
+string a, b;
+int main(){
+    cin >> a >> b; 
+    cout << a << "\n"; 
+    cout << b << "\n"; 
+    return 0;
+}
+/*
+입력
+빨간 사과
+
+출력 
+빨간
+사과
+*/
