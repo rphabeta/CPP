4605a875aa3b1babe9a06d09665a2ebf19528657
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-29 23:38:59 +0900

    입력(cin, scan)

---
+++ BasicSyntax/IO/CinAndScanf.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int a;
+int main(){
+    cin >> a; 
+    scanf("%d", &a); 
+    return 0;
+}
+// cin은 개행문자(띄어쓰기, 엔터)직전 까지 입력받음
+// scanf는 형식을 지정해서 입력 받음
+// 따라서, 형식을 기반으로 입력이 주어지지 않은 경우 scanf보다는 cin을 쓰기 지향
