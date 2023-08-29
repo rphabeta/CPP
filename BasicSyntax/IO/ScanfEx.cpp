63985d78ddb6e7d07f6a2533b3569e23d30c177a
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-29 23:45:38 +0900

    scanf 입력 예시

---
+++ BasicSyntax/IO/ScanfEx.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std; int a;
+double b;
+char c;
+int main(){
+    scanf("%d %lf %c", &a, &b, &c); 
+    printf("%d\n", a);
+    printf("%lf\n", b); 
+    printf("%c\n", c);
+    return 0; 
+}
+
+/*
+%d는 int 타입.
+%lf는 double 타입.
+%c는 char 타입.
+*/
+
+#-----------------
+
+/*
+입력
+23330
+233.23123
+a
+
+출력
+23330
+233.231230
+a
+*/
