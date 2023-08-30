72a1537b0f3b1cbfa2c515bb4011cf231cb34230
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-30 20:31:42 +0900

    다양한 형식 지정을 통한 출력(printf)

---
+++ BasicSyntax/IO/PrintfFormatting.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int a = 1;
+char s = 'a';
+string str = "지금까지 이런 치킨은 없었다"; 
+double b = 1.223123;
+
+int main(){
+    printf("극한직업 : %d\n",a); 
+    printf("극한직업 : %c\n",s); 
+    printf("극한직업 : %s\n",str.c_str()); 
+    printf("극한직업 : %lf\n",b);
+    return 0;
+}
+/*
+극한직업 : 1 
+극한직업 : a 
+극한직업 : 지금까지 이런 치킨은 없었다 
+극한직업 : 1.223123 
+*/
