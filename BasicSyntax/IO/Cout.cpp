4b47b76bafc8033b96861cd88bb1bc4c9ab8705f
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-30 20:20:34 +0900

    단순 출력(cout)

---
+++ BasicSyntax/IO/Cout.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+string a = "금요일이다!"; 
+string b = "금요일 너무 좋다..."; 
+int main(){
+    cout << a << '\n';
+    cout << a << " " << "" << b << '\n'; 
+    return 0;
+}
+/*
+금요일이다!
+금요일이다! 금요일 너무 좋다...
+*/
