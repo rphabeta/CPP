916c73ac78aa134f317971d3e0931c75b410163b
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-31 13:56:12 +0900

    문자열 선언 (string)

---
+++ BasicSyntax/Type/StringAndCharArray.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main(){
+    string a = "금요일~";
+    cout << a[0] << "\n";
+    cout << a[0] << a[1] << a[2] << '\n'; cout << a << "\n";
+    string b = "abc";
+    cout << b[0] << "\n";
+    cout << b << "\n";
+    return 0;
+}
+
+/* 
+?
+나 
+나는야 
+a
+abc 
+*/
+// 문자열을 선언하는 방법으로는 크게 char[]과 string 을 통해서 선언하는 두 가지 방법이 존재한다.
+// 이중, struing을 사용하여 처리하는 것을 추천하는 편이다.
