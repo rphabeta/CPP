84b2bc73b457e538ad788a299c22cb42377794a6
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-08-31 14:02:54 +0900

    특정 자릿수까지 출력(precision)

--- BasicSyntax/IO/CoutFloat.cpp
+++ BasicSyntax/IO/CoutFloat.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std; 
 typedef long long ll; 
-double a = 1.23456789; 
+double a = 1.2345678; 
 int main(){
     cout << a << "\n"; // 1.23457 
     cout.precision(7); // cout.precision(자릿수 + 1);
