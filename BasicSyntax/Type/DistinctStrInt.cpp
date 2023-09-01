5f9eccb052120cc46b67aa3e989193e04431540c
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-09-01 16:21:25 +0900

    문자열 숫자 판별(atoi(변수.c_str()))

---
+++ BasicSyntax/Type/DistinctStrInt.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main(){
+    string s = "1";
+    string s2 = "amumu";
+    cout << atoi(s.c_str()) << '\n'; 
+    cout << atoi(s2.c_str()) << '\n'; 
+    return 0;
+}
+/* 
+1 
+0 
+*/
+
+// 입력받은 값이 문자열인지? 숫자인지?
+// atoi(변수.c_str()) 을 통해서 변수가 숫자라면 return 0
