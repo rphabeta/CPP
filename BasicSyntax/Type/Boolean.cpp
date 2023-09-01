8361c6e77952a3ab88c0539b7427a697d0850fbb
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-09-01 16:25:18 +0900

    Bool 형식

---
+++ BasicSyntax/Type/Boolean.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL); 
+    int a = -1;
+    cout << bool(a) << "\n";
+    a = 0;
+    cout << bool(a) << "\n"; a = 3;
+    cout << bool(a) << "\n";
+}
+/* 
+1 
+0 
+1 
+*/
+
+// C++에서는 0이면 false, 0이 아닌 값들은 모두 true
